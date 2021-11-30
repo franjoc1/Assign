de488ca7dd1868f65d5f24081a067fa0252fe8cb
franjoc1 <95301997+franjoc1@users.noreply.github.com>
2021-11-30 17:07:28 +0100

    Delete API_GET_REQUEST/features directory

--- API_GET_REQUEST/features/environment.py
+++
@@ -1,9 +0,0 @@
-def before_tag(context, tag):
-    if 'tag1' in str(tag):
-        print ('=Before Tag=' + str(tag))
-
-
-def after_tag(context, tag):
-    if 'tag1' in str(tag):
-        print ('=After Tag=' + str(tag))
-
